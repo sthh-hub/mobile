0478bb36ab6086908af506eaa38503ff63376eef
ST <147098993+sthh-hub@users.noreply.github.com>
2024-07-04 13:51:21 -0700

    add header

--- App.js
+++ App.js
@@ -2,9 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const appName = "Summer 2024 class";
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!!!!</Text>
+      <Header />
+      <Text>Welcome to {appName}</Text>
       <StatusBar style="auto" />
     </View>
   );
