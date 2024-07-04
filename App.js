6133de00e0d70ebe9e27e61911e3ff14bae9aff3
ST <147098993+sthh-hub@users.noreply.github.com>
2024-07-04 14:47:41 -0700

    add textinput compo

--- App.js
+++ App.js
@@ -1,17 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
 import Header from "./Header";
 
 export default function App() {
   const appName = "Summer 2024 class";
+  const [text, setText] = useState('');
 
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Header name = {appName} theme="dark">
-        <Text> This is a child component</Text>
-        <Text> This is another child component</Text>
+        {/* <Text> This is a child component</Text>
+        <Text> This is another child component</Text> */}
       </Header>
+      <TextInput 
+        value={text}
+        onChangeText={function (changedText) 
+          {
+            console.log("changedText: ", changedText);
+            setText(changedText);
+            console.log("text: ", text);
+          }}
+        placeholder="Enter your name" 
+        autoCapitalize={true}
+        />
       <StatusBar style="auto" />
     </View>
   );
