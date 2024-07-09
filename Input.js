127eddd619c1aec0d3877873712f5c09456da388
ST <147098993+sthh-hub@users.noreply.github.com>
2024-07-09 13:40:31 -0700

    confirm button and handleConfirm function

--- Input.js
+++ Input.js
@@ -1,28 +1,35 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, Button } from 'react-native';
 
 export default function Input() {
     const [text, setText] = useState('');
     const [thankYouVisible, setThankYouVisible] = useState(false);
 
+    function handleConfirm() {
+        console.log("user typed: ", text);
+        setText('');
+    };
+
+    function changedText(changedText) {
+        console.log("changedText: ", changedText);
+        setText(changedText);  
+        console.log("text: ", text);
+        setThankYouVisible(false);
+    }
+
     return (
         <View>
             <TextInput
                 value={text}
-                onChangeText={function (changedText) {
-                    console.log("changedText: ", changedText);
-                    setText(changedText);
-                    console.log("text: ", text);
-                    setThankYouVisible(false);
-                }}
+                onChangeText={() => changedText()}
                 onBlur={() => setThankYouVisible(true)} 
                 placeholder="Enter your name"
                 autoCapitalize={true}
                 autoFocus={true}
             >
             </ TextInput>
-            {thankYouVisible && <Text >Thank you!</Text>}
-            <Text>Your last input: {text}</Text>
+            {thankYouVisible && <Text >Thank you! {text}</Text>}
+            <Button title="Confirm" onPress={()=>{handleConfirm();}}></Button>
         </View>
     );
 }
