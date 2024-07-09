c60337bec9fc405afcf1bc23f20c6e567c549020
ST <147098993+sthh-hub@users.noreply.github.com>
2024-07-09 14:26:23 -0700

    set modal animation slide and toggle visible

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import React, { useState } from 'react';
 import Header from "./Header";
 import Input from "./Input";
@@ -8,11 +8,13 @@ export default function App() {
   const appName = "Summer 2024 class";
 
   const [receivedText, setReceivedText] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
 
   // To receive data add a parameter
   function handleInputData(data) {
     console.log("callback fn called from child component with data: ", data);
     setReceivedText(data);
+    setModalVisible(false);
   }
 
 
@@ -23,8 +25,9 @@ export default function App() {
         {/* <Text> This is a child component</Text>
         <Text> This is another child component</Text> */}
       </Header>
+      <Button title="Add a goal" onPress={() => {setModalVisible(true)}}></Button>
       {/* set up a callback function */}
-      <Input inputHandler={handleInputData}/> 
+      <Input inputHandler={handleInputData} isModalVisible={modalVisible}/> 
       {/* use the state variable to render the received data */}
       <Text>{receivedText}</Text>
       <StatusBar style="auto" />
--- Input.js
+++ Input.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, Button, Modal, StyleSheet } from 'react-native';
 
 // two ways to pass props!
 // export default function Input( props ) {
 //     console.log(props.inputHandler);
-export default function Input( {inputHandler} ) {
+export default function Input({ inputHandler, isModalVisible }) {
     console.log(inputHandler);
     const [text, setText] = useState('');
     const [thankYouVisible, setThankYouVisible] = useState(false);
@@ -13,30 +13,40 @@ export default function Input( {inputHandler} ) {
         console.log("user typed: ", text);
         inputHandler(text); // pass data to parent
         setThankYouVisible(true);
-        setText('');
     };
 
     function changedText(changedText) {
         console.log("changedText: ", changedText);
-        setText(changedText);  
+        setText(changedText);
         console.log("text: ", text);
         setThankYouVisible(false);
     }
 
     return (
-        <View>
-            <TextInput
-                value={text}
-                // onChangeText={(changedText)=>changedText(changedText)} // we need to have a variable to store the changed text
-                onChangeText={changedText} // same as above
-                onBlur={() => setThankYouVisible(true)} 
-                placeholder="Enter your name"
-                autoCapitalize={true}
-                autoFocus={true}
-            >
-            </ TextInput>
-            {thankYouVisible && <Text >Thank you!</Text>}
-            <Button title="Confirm" onPress={()=>{handleConfirm();}}></Button>
-        </View>
+        <Modal animationType="slide" visible={isModalVisible}>
+            <View style={styles.container}>
+                <TextInput
+                    value={text}
+                    // onChangeText={(changedText)=>changedText(changedText)} // we need to have a variable to store the changed text
+                    onChangeText={changedText} // same as above
+                    onBlur={() => setThankYouVisible(true)}
+                    placeholder="Enter your name"
+                    autoCapitalize={true}
+                    autoFocus={true}
+                >
+                </ TextInput>
+                {thankYouVisible && <Text >Thank you!</Text>}
+                <Button title="Confirm" onPress={() => { handleConfirm(); }}></Button>
+            </View>
+        </Modal>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
